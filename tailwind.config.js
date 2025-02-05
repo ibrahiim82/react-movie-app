77f0b3c1d52abf90d906050a71ece32ead723b45
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-02-05 17:23:56 +0300

    lastcommit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   //? kendi theme ayarlarımızı eklemek için bu kısmı ekliyoruz
   theme: {
-    //! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
+    //! ÖNEMLİ!! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
     extend: {
       colors: {
         "gray-dark-main": "#23242a",
